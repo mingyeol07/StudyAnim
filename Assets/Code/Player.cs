f616aefa0624f413ccdca8c47725144a27702aca
Mingyeol Kim <neomingyeol@gmail.com>
2024-04-15 00:25:20 +0900

    jump

--- Assets/Code/Player.cs
+++ Assets/Code/Player.cs
@@ -7,115 +7,127 @@ using UnityEngine.UIElements;
 public class Player : MonoBehaviour
 {
     [Header("Move")]
-    [SerializeField] private float _moveSpeed;
-    private float _moveInput;
+    [SerializeField] private float moveSpeed;
+    private float moveInput;
 
     [Header("Jump")]
-    private bool _isGrounded;
-    [SerializeField] private Transform _feetPos;
-    private float _checkRadius = 0.1f;
-    [SerializeField] private LayerMask _whatIsGround;
-    [SerializeField] private float _jumpForce;
+    private bool isGrounded;
+    [SerializeField] private Transform feetPos;
+    private float checkRadius = 0.1f;
+    [SerializeField] private LayerMask whatIsGround;
+    [SerializeField] private float jumpForce;
 
     [Header("Attack")]
-    private bool _isAttack;
-    private bool _comboAttack;
+    private bool isAttack;
+    private bool comboAttack;
 
     [Header("Roll")]
-    private bool _isRolling;
-    [SerializeField] float _rollSpeed;
+    private bool isRolling;
+    [SerializeField] float rollSpeed;
 
-    private Rigidbody2D _rb;
-    private Animator _anim;
+    private Rigidbody2D rigid;
+    private Animator anim;
 
     private void Start()
     {
-        _anim = GetComponent<Animator>();
-        _rb = GetComponent<Rigidbody2D>();
+        anim = GetComponent<Animator>();
+        rigid = GetComponent<Rigidbody2D>();
     }
 
     private void Update()
     {
         ValueSet();
 
-        if (Input.GetKeyDown(KeyCode.X) && !_isRolling)
+        if (Input.GetKeyDown(KeyCode.X) && !isRolling && isGrounded)
         {
             Attack();
         }
-        else if (_isRolling && !_isAttack)
+        else if (isRolling && !isAttack)
         {
-            _rb.velocity = transform.right * _rollSpeed;
+            rigid.velocity = transform.right * rollSpeed;
         }
 
-        if (!_isAttack && !_isRolling)
+        if (!isAttack && !isRolling)
         {
             Move();
-            if(_isGrounded )
+            if(isGrounded)
             {
                 if (Input.GetKeyDown(KeyCode.LeftShift)) Roll();
-                if (Input.GetKeyDown(KeyCode.Space)) Jump();
+                if (Input.GetKeyDown(KeyCode.Space) && !anim.GetBool("IsJumping")) Jump();
+            }
+
+            if (anim.GetBool("IsJumping")){
+                anim.SetFloat("Velocity", rigid.velocity.y);
+                if (rigid.velocity.y < 0)
+                {
+                    
+                    anim.SetBool("IsJumping", !isGrounded);
+                }
             }
         }
     }
     
     private void ValueSet() // 바라보는 방향, 땅에 닿았는지
     {
-        _moveInput = Input.GetAxisRaw("Horizontal");
-        _anim.SetBool("IsRun", Mathf.Approximately(_moveInput, 0) ? false : true);
-        _isGrounded = Physics2D.OverlapCircle(_feetPos.position, _checkRadius, _whatIsGround);
+        moveInput = Input.GetAxisRaw("Horizontal");
+        anim.SetBool("IsRunning", Mathf.Approximately(moveInput, 0) ? false : true);
+        isGrounded = Physics2D.OverlapCircle(feetPos.position, checkRadius, whatIsGround);
 
-        if(_isAttack) _rb.velocity = Vector3.zero;
+        if(isAttack) rigid.velocity = Vector3.zero;
     }
 
     private void Move()
     {
-        _rb.velocity = new Vector2(_moveSpeed * _moveInput, _rb.velocity.y);
+        rigid.velocity = new Vector2(moveSpeed * moveInput, rigid.velocity.y);
 
-        if (_moveInput > 0) { transform.eulerAngles = Vector3.zero; }
-        else if (_moveInput < 0) transform.eulerAngles = new Vector3(0, 180, 0);
+        if (moveInput > 0) { transform.eulerAngles = Vector3.zero; }
+        else if (moveInput < 0) transform.eulerAngles = new Vector3(0, 180, 0);
     }
 
     private void Jump()
     {
-        _rb.AddForce(Vector2.up * _jumpForce, ForceMode2D.Impulse);
+        anim.SetBool("IsJumping", true);
+        rigid.AddForce(Vector2.up * jumpForce, ForceMode2D.Impulse);
     }
 
     private void Roll()
     {
-        _isRolling = true;
-        _anim.SetBool("IsRolling", true); 
-    }
-
-    private void RollingExit() // AnimEvent
-    {
-        _isRolling = false;
-        _anim.SetBool("IsRolling", false);
-        _rb.velocity = Vector2.zero;
+        isRolling = true;
+        anim.SetBool("IsRolling", true); 
     }
 
     private void Attack()
     {
-        if (_isAttack == false)
+        if (isAttack == false)
         {
-            _isAttack = true;
-            _anim.SetTrigger("AttackTrigger");
+            isAttack = true;
+            anim.SetTrigger("AttackTrigger");
         }
-        else if (_isAttack == true && _comboAttack == true)
+        else if (isAttack == true && comboAttack == true)
         {
-            _anim.SetBool("AttackCombo", true);
+            anim.SetBool("AttackCombo", true);
         }
     }
 
-    private void OnComboAttack()
+    #region AnimEvent
+    private void RollingExit() // AnimEvent
     {
-        _comboAttack = true;
+        isRolling = false;
+        anim.SetBool("IsRolling", false);
+        rigid.velocity = Vector2.zero;
+    }
+
+    private void OnComboAttack() // AnimEvent
+    {
+        comboAttack = true;
     }
 
     private void AttackExit() // AnimEvent
     {
-        _isAttack = false;
-        _comboAttack = false;
-        _anim.SetBool("AttackCombo", false);
-        _anim.SetBool("IsRun", false);
+        isAttack = false;
+        comboAttack = false;
+        anim.SetBool("AttackCombo", false);
+        anim.SetBool("IsRunning", false);
     }
+    #endregion
 }
