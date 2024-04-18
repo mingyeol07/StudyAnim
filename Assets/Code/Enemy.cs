27058835c1a89706565adf7751dd5661493afe83
김민결 <neomingyeol@gmail.com>
2024-04-18 09:49:27 +0900

    assets

---
+++ Assets/Code/Enemy.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Security.Authentication.ExtendedProtection;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    private int hp;
+
+    protected private void OnHit()
+    {
+
+    }
+}
